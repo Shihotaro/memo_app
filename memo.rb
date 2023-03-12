e8c54daf403fb264245e27ebabdf8fee32cf053f
Shihotaro <etoile02@icloud.com>
2023-03-12 17:18:02 +0900

    修正版

--- memo.rb
+++ memo.rb
@@ -2,30 +2,42 @@ require 'csv'
 
 puts '1 → 新規でメモを作成する / 2 → 既存のメモを編集する'
 
-# # ユーザーのキーワード入力を待機
 input_val = gets.to_i
 
 if input_val == 1
-    puts '拡張子を除いたファイル名を入力してください。'
-    new_name = gets.chomp
-    File.write("#{new_name}.csv", <<CSV)
-CSV
-    puts 'メモしたい内容を入力してください。'
+  puts '拡張子を除いたファイル名を入力してください。'
+  #   新規ファイルの作成
+  new_name = gets.chomp
+  File.new("#{new_name}.csv", 'w')
 
-    CSV.read("#{new_name}.csv", headers: false)
+  puts 'メモしたい内容を入力してください。'
+  puts '完了したらCtrl+Dをしてください。'
 
-    CSV.open("#{new_name}.csv", 'w') do |test|
-        memo = gets.chomp
-        test << [memo]
-    end
+  #   新規ファイルに書き込み
+  memos = $stdin.readlines
+  CSV.open("#{new_name}.csv", 'w') do |test|
+    for memo in memos do
+      test << [memo.chomp]
+      end
+  end
 
 elsif input_val == 2
-    puts '編集したい既存のファイル名を入力してください。'
-    file_name = gets.chomp
+  puts '編集したい既存のファイル名を入力してください。(拡張子除く)'
+  file_name = gets.chomp
 
-    CSV.open("#{file_name}", 'w') do |test|
-    test << [gets.chomp]
+  puts 'メモしたい内容を入力してください。'
+  puts '完了したらCtrl+Dをしてください。'
+
+  # 既存ファイルデータの読み込み
+  #   puts CSV.read("#{file_name}.csv")
+
+  # 既存ファイルに書き込み
+  memos = $stdin.readlines
+  CSV.open("#{file_name}.csv", 'a') do |test|
+    for memo in memos do
+      test << [memo.chomp]
     end
+  end
 else
-    puts '"1"か"2"を入力してください。'
+  puts '"1"か"2"を入力してください。'
 end
